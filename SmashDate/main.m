cb20d838652824fcbba3fdb185d05a584a83e109
John Gambrell <john.gambrell@gmail.com>
2014-05-29 10:10:56 -0500

    everything

---
+++ SmashDate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmashDate
+//
+//  Created by Gambrell, John on 5/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGAppDelegate class]));
+    }
+}
