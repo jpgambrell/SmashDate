fae0c97125dc9ba21174cfb2ef243118ff6560e9
John Gambrell <john.gambrell@gmail.com>
2014-06-17 16:15:33 -0500

    new form sb design

---
+++ SmashDate/DateContactFormTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DateContactFormTableViewController.h
+//  SmashDate
+//
+//  Created by Gambrell, John on 6/17/14.
+//  Copyright (c) 2014 Gambrell, John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateContactFormTableViewController : UITableViewController
+
+@end
